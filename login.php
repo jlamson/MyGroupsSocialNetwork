129d3402ff6e5c9aa2a7bfba9d818e83c7811447
Joshua Lamson <jlamson@mymail.mines.edu>
2012-11-18 20:14:55 -0700

    added login page

---
+++ login.php
@@ -0,0 +1,21 @@
+<DOCTYPE html>
+<html>
+<head>
+	<title>myGroups</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body>
+	<h1>myGroups</h1>
+	<h2>Login</h2>
+	<form action="home.php">
+		<label for="loginEmail">Email:</label>
+			<input name="loginEmail" id="loginEmail" type="email" /> 
+		<br />
+		<label for="loginPassword">Pasword</label>
+			<input name="loginPassword" id="loginPassword" type="password" />
+		<br />
+		<input type="submit" value="Login" />
+	</form>
+	<p><a href="newProfile.php">New User?</a></p>
+</body>
+</html>
