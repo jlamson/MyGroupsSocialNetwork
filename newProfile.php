75f36f1393812dbeb3c4c31ae245775ae6996397
Jon Waldow <jwaldow@mines.edu>
2012-11-18 19:51:56 -0700

    adding new profile page

---
+++ newProfile.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>MyGroups</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+</body>
+</html>
+
