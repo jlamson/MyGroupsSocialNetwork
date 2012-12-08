6e603707b3321bfc8bbe64fc1cd243189e4880a0
Jon Waldow <jwaldow@mines.edu>
2012-12-08 03:39:27 -0700

    working on home and view profile

--- home.html.php
+++ home.html.php
@@ -5,25 +5,30 @@
 </form>
 <hr /><hr /><hr />
 
-<?php foreach ($posts as $post) { ?>
-	<p>
-		<span class="status_header"><?= $post['username'] ?>: </span>
-		<span class="status_content"><?= $post['status'] ?> </span>
-	</p>
-	<?php if ($post['comments']) { ?>
-		<p class="comment_block">
-			<?php foreach ($post['comments'] as $comment) { ?>
-				<span class="comment_header"><?= $comment['username'] ?>: </span>
-				<span class="comment_content"><?= $comment['comment'] ?> </span>
-				<br />
-			<?php } ?>
+<?php 
+	if ($posts == array()){
+		echo "You have no posts";
+	} else {
+		foreach ($posts as $post) { ?>
+		<p>
+			<span class="status_header"><?= $post['username'] ?>: </span>
+			<span class="status_content"><?= $post['status'] ?> </span>
 		</p>
-	<?php } ?>
-	<form action="" method="post">
-		<input class="status_comment" name="status_comment" type="text" />
-		<input type="hidden" name="post_id" value="<?= $post['id'] ?>" />
-		<input class="submit_status_comment" name="comment" type="submit" value="Comment" />
-	</form>
-	<hr />
-<?php } ?>
+		<?php if ($post['comments']) { ?>
+			<p class="comment_block">
+				<?php foreach ($post['comments'] as $comment) { ?>
+					<span class="comment_header"><?= $comment['username'] ?>: </span>
+					<span class="comment_content"><?= $comment['comment'] ?> </span>
+					<br />
+				<?php } ?>
+			</p>
+		<?php } ?>
+		<form action="" method="post">
+			<input class="status_comment" name="status_comment" type="text" />
+			<input type="hidden" name="post_id" value="<?= $post['id'] ?>" />
+			<input class="submit_status_comment" name="comment" type="submit" value="Comment" />
+		</form>
+		<hr />
+<?php } 
+}?>
 
