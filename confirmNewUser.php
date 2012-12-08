b584cca2bd0c9690dd359b01d207d89fdd330146
jlamson <jlamson@mymail.mines.edu>
2012-12-08 03:20:46 -0700

    ensured new user is UNIQUE! Also cleaned up new User process

--- confirmNewUser.php
+++ confirmNewUser.php
@@ -2,13 +2,13 @@
 <?php
 	include 'phpHelper.php';
 	$db = initDB();
+	$error = "";
 
-	$DOCUMENT_ROOT = $_SERVER['DOCUMENT_ROOT'];
 	$userName = $_POST['uname'];
 	$firstName = $_POST['fname'];
 	$lastName = $_POST['lname'];
 	$email = $_POST['email'];
-	$email = $_POST['emailConfirm'];
+	$email_confirm = $_POST['emailConfirm'];
 	$password = $_POST['password'];
 	$passwordConfirm = $_POST['passwordConfirm'];
 	$gender = $_POST['gender'];
@@ -18,19 +18,35 @@
 	
 	$pwHash = md5($password);
 
-	$insert = "INSERT INTO users (email, username, first_name, last_name, password, gender, month, day, year) 
-		VALUES (?,?,?,?,?,?,?,?,?)";
 	
-	$stmt = $db->prepare($insert);
-	$stmt->bind_param("ssssssiii", $email, $userName, $firstName, $lastName, $pwHash, $gender, $month, $day, $year);	
-	$stmt->execute();
-		
-    if($stmt->affected_rows == 1) {
-		$title = "Welcome $firstName";
-		$include = "confirmNewUser.html.php";
-    	include 'layout.php';
-    } else {
-    	$error = "Query Failed (" . $stmt->errno . ") " . $stmt->error;
-    }
+	if($email != $email_confirm) {
+		$error = "The email and confirm email did not match!";
+	} else if ($password != $passwordConfirm) {
+		$error = "The password and confirm password did not match!";
+	}  
 
+	if($error ==  "") {
+		$error = isUniqueUser($userName, $email);
+	}
+
+	if($error == "") { 
+		$insert = "INSERT INTO users (email, username, first_name, last_name, password, gender, month, day, year) 
+			VALUES (?,?,?,?,?,?,?,?,?)";
+		
+		$stmt = $db->prepare($insert);
+		$stmt->bind_param("ssssssiii", $email, $userName, $firstName, $lastName, $pwHash, $gender, $month, $day, $year);	
+		$stmt->execute();
+			
+	    if($stmt->affected_rows == 1) {
+			$title = "Welcome $firstName";
+			$include = "confirmNewUser.html.php";
+	    	include 'layout.php';
+	    } else {
+	    	$error = "Query Failed (" . $stmt->errno . ") " . $stmt->error;
+	    }
+	} else {
+		$title = "Error";
+		$include = "confirmNewUser.html.php";
+	    include 'layout.php';
+	}
 ?>
