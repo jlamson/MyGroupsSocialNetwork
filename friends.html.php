af72ac1c84eea99d7bef601b557e577824b8f0b5
Jon Waldow <jwaldow@mines.edu>
2012-12-08 01:48:31 -0700

    finished friends page

--- friends.html.php
+++ friends.html.php
@@ -1,28 +1,25 @@
 <!-- friends.html.php -->
 <h1>FRIENDS!!</h1>
-<ul>
-	<li>post foo blah</li>
-	<li><ul>
-		<li>comment 1</li>
-		<li>comment 2</li>
-		<li>comment 3</li>
-	</ul></li>
-	<li>post foo blah</li>
-	<li><ul>
-		<li>comment 1</li>
-		<li>comment 2</li>
-		<li>comment 3</li>
-	</ul></li>
-	<li>post foo blah</li>
-	<li><ul>
-		<li>comment 1</li>
-		<li>comment 2</li>
-		<li>comment 3</li>
-	</ul></li>
-	<li>post foo blah</li>
-	<li><ul>
-		<li>comment 1</li>
-		<li>comment 2</li>
-		<li>comment 3</li>
-	</ul></li>
-</ul>
+<?php
+
+		foreach($all_info as $cur_mem){
+?>
+			<h1><?php echo $cur_mem["username"]; ?></h1>
+			<h2>About Me<h2>
+			<p><?php echo $cur_mem["about"] ?></p>
+			<h2>Birthday</h2>
+			<p><?php echo $cur_mem["month"]."/".$cur_mem["day"]."/".$cur_mem["year"] ?></p>
+			<form action="" method="post">
+
+			<input type="hidden" name="friendId" id ="friendId" value=<?php echo $cur_mem["id"]; ?> />
+			<!-- 	//checks to see if already a freind, then gives option to delete. -->
+			<?php 
+			if((getFriendIds()!=false)&&(in_array($cur_mem["id"], getFriendIds()))){
+			?>
+				<input class="submit" name="deleteFriend" type="submit" value="Delete Friend" />
+			<?php }else{ ?>
+				<input class="submit" name="addFriend" type="submit" value="Add as Friend" />
+			<?php } ?>
+			</form>
+			<hr>
+	<?php	} ?>
