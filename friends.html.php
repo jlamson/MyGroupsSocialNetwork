047f071bcb100bc46d41a370a774609d8ee39559
Jon Waldow <jwaldow@mines.edu>
2012-12-09 20:44:47 -0700

    added note about what to search by

--- friends.html.php
+++ friends.html.php
@@ -3,6 +3,7 @@
 <form action="" method="post" >
 	<input class="term" name="search_term" type="text" />
 	<input class="submit_term" name="search" type="submit" value="Search" />
+	<p>* Search by email, user name, first name, or last name</p>
 </form>
 <?php if ($all_info == array()) { ?>
 	<p>You have no friends that matched your search</p>
